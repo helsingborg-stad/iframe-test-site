b2120134db906703a8e772883cb451cc4c612be1
Petter Andersson <40427478+petter-a@users.noreply.github.com>
2026-01-19 09:33:13 +0100

    Move stylesheet link creation inside iframe check

--- iframe.js
+++ iframe.js
@@ -1,8 +1,9 @@
-const l = document.createElement("link");
-l.rel = "stylesheet"; l.href = "https://iframe.municipio.tech/styles.css";
-document.head.appendChild(l);
 
 if (window.location !== window.parent.location) {
+  const l = document.createElement("link");
+  l.rel = "stylesheet"; l.href = "https://iframe.municipio.tech/styles.css";
+  document.head.appendChild(l);
+
   document.querySelector("html, body").style.height = "auto";
   const sendMessageToParent = () =>
     window.parent.postMessage({ height: document.body.clientHeight }, '*');
