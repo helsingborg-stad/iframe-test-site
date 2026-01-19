2fee7f93f1870f318a6e6e35988cec9c5de8ad50
Petter Andersson <40427478+petter-a@users.noreply.github.com>
2026-01-19 09:02:59 +0100

    Add support for resizing iframe based on content

--- iframe.js
+++ iframe.js
@@ -1,3 +1,11 @@
 const l = document.createElement("link");
 l.rel = "stylesheet"; l.href = "https://iframe.municipio.tech/styles.css";
 document.head.appendChild(l);
+
+if (window.location !== window.parent.location) {
+  document.querySelector("html, body").style.height = "auto";
+  const sendMessageToParent = () =>
+    window.parent.postMessage({ height: document.body.clientHeight }, '*');
+  sendMessageToParent();
+  new ResizeObserver(() => sendMessageToParent()).observe(document.body);
+}
