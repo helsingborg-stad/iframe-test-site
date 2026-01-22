a126590434a1beed87744f6aa3f8e5b0e4b48c0d
Petter Andersson <40427478+petter-a@users.noreply.github.com>
2026-01-22 09:39:39 +0100

    Hide header element in iframe.js

--- iframe.js
+++ iframe.js
@@ -1,3 +1,4 @@
 import { registerIFrameClient } from './module.js';
 
+document.getElementsByTagName("header")[0].style.display = 'none';
 registerIFrameClient("https://e-service.helsingborg.se", window);
