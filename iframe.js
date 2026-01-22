f658df3df4158e11fddd98a67821575e5145d05c
Petter Andersson <40427478+petter-a@users.noreply.github.com>
2026-01-22 09:48:21 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,4 +1,3 @@
 import { registerIFrameClient } from './module.js';
 
-document.getElementsByTagName("header")[0].style.display = 'none';
 registerIFrameClient("https://e-service.helsingborg.se", window);
