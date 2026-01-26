141b68c79894cd87631b07ac34401c96d746ffba
Petter Andersson <40427478+petter-a@users.noreply.github.com>
2026-01-26 15:16:26 +0100

    Correct map function syntax for CSS variable extraction
    Fix syntax error in map function for CSS variables.

--- assets/js/module.js
+++ assets/js/module.js
@@ -22,9 +22,9 @@ export const registerIFrameHost = ({ id, assets, allowedOrigin }, window) => {
             });
             iframe.onload = () => {
                 var variables = getComputedStyle(document.documentElement);
-                var r = Array.from(getComputedStyle(document.documentElement)).filter(x => x.startsWith('--')).map(x => x) {
-                    `${x}; ${variables}.getPropertyValue(x)
-                }
+                var r = Array.from(getComputedStyle(document.documentElement)).filter(x => x.startsWith('--')).map(x =>
+                    `${x}; ${variables}.getPropertyValue(x);
+                );
                 assets += `:root {\n${r.join(';\n')}}`;
                 console.log(assets);
                 var _a;
